160d72a2b7af204ba938f8424f008a410133fad0
r0302765 <simon.telen@student.kuleuven.be>
2013-03-25 09:59:25 +0100

    asteroid

--- src/asteroids/model/Bullet.java
+++ src/asteroids/model/Bullet.java
@@ -14,12 +14,6 @@ public class Bullet extends SpaceObject {
 
 	// The ship that fired this bullet.
 	private final Ship source;
-	// The position of this bullet.
-	private Position pos;
-	// The velocity of this bullet.
-	private Velocity vel;
-	// The radius of this bullet.
-	private double radius;
 	// The density of a bullet in kg/km³.
 	private static final double RHO_BULLET = 7.8 * Math.pow(10, 12);
 
@@ -65,55 +59,6 @@ public class Bullet extends SpaceObject {
 
 	}
 
-	/**
-	 * Returns the position of this bullet.
-	 */
-	@Basic
-	public Position getPos() {
-		
-		return this.pos;
-		
-	}
-
-	/**
-	 * Set the pos of this bullet to the given pos.
-	 */
-	@Basic
-	public void setPos(Position pos) throws IllegalPositionException {
-		
-		super.setPos(pos);
-		
-	}
-
-	/**
-	 * Return the vel of this bullet.
-	 */
-	@Basic
-	public Velocity getVel() {
-		
-		return this.vel;
-		
-	}
-
-	/**
-	 * Set the vel of this bullet to the given vel.
-	 */
-	@Basic
-	public void setVel(Velocity vel) {
-		
-		super.setVel(vel);
-		
-	}
-
-	/**
-	 * returns the radius of this bullet.
-	 */
-	@Basic
-	public double getRadius() {
-		
-		return this.radius;
-		
-	}
 
 
 	/**
