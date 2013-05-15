411649b973cb8eb7703ec0aff237a01f3ddcc3fc
Bruno Coussement <coussemetn.bruno@gmail.com>
2013-05-15 14:46:12 +0200

    uhygfc

--- src/asteroids/model/programs/ITernaryStructure.java
+++ src/asteroids/model/programs/ITernaryStructure.java
@@ -1,8 +1,10 @@
 package asteroids.model.programs;
 
+import asteroids.Error.IllegalOperandException;
+
 public interface ITernaryStructure extends IBinaryStructure {
 
 	public abstract IEntry getThirdOperand();
-	public abstract void setThirdOperand(IEntry tOperand);
+	public abstract void setThirdOperand(IEntry tOperand) throws IllegalOperandException;
 	public abstract boolean canHaveAsThirdOperand(IEntry tOperand);
 }
--- src/asteroids/model/programs/IUnaryStructure.java
+++ src/asteroids/model/programs/IUnaryStructure.java
@@ -1,9 +1,11 @@
 package asteroids.model.programs;
 
+import asteroids.Error.IllegalOperandException;
+
 public interface IUnaryStructure extends IComposedStructure {
  
 	public abstract IEntry getOperand();
-	public abstract void setOperand(IEntry operand);
+	public abstract void setOperand(IEntry operand) throws IllegalOperandException;
 	public abstract boolean canHaveAsOperand();
 	
 }
