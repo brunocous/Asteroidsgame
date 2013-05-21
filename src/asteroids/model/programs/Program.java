1919a34d4dc0d63febcf9f0024d94d3f2ea9df60
Bruno Coussement <coussemetn.bruno@gmail.com>
2013-05-21 18:13:09 +0200

    fghj

--- src/asteroids/model/programs/Program.java
+++ src/asteroids/model/programs/Program.java
@@ -86,6 +86,7 @@ public class Program {
 	public Map<String,Variable> convertToVariables(Map<String,Type> globals){
 		HashMap<String,Variable> result = new HashMap<String,Variable>();
 		for(String name: globals.keySet()){
+			System.out.println("naam van variabele: " + name + " en zijn type: " + globals.get(name));
 			result.put(name, new Variable(name,globals.get(name)));
 		}
 		return result;
--- src/asteroids/model/programs/expressions/Variable.java
+++ src/asteroids/model/programs/expressions/Variable.java
@@ -14,92 +14,94 @@ private final String name;
 private Type type;
 
 public Variable(String name){
-	
-	if(!canHaveAsName(name)){
-		this.name = "defaultName";
-	}
-	else{
-	this.name = name;
-	}
-	this.value= null;
-	this.type = null;
+
+if(!canHaveAsName(name)){
+this.name = "defaultName";
+}
+else{
+this.name = name;
+}
+this.value= null;
+this.type = null;
 }
 public Variable(String name, Type type){
-	this(name);
-	this.type = type;
+this(name);
+this.type = type;
 }
-	
+
 public void setValue(Expression value) {
-	if(canHaveAsValue(value)){
-		this.value = value;
-	}
-	else{
-	
-	}
+if(value!= null){
+if(canHaveAsValue(value)){
+this.value = value.getValue();
+}
+}
+else{
+this.value = null;
+}
 }
 
 public boolean canHaveAsValue(Expression value){
-	
-	return true;
+
+return true;
 
 }
 public String getName(){
-	return name;
+return name;
 }
 public boolean canHaveAsName(String name){
-	return name != null && name != "";
+return name != null && name != "";
 }
 public Expression getValue(){
-	return value.getValue();
+return value;
 }
-public Expression getVariableValue(){
-	return value;
-}
+
 
 @Override
 public Object getRealValue() {
-	if(getValue().getType()== Type.DOUBLE){
-		return new DoubleLiteral(((DoubleLiteral)(value.getValue())).getRealValue());
-	}
-	else if(getValue().getType()== Type.BOOLEAN){
-		return new BooleanLiteral(((BooleanLiteral)(value.getValue())).getRealValue());
-	}
-	else if(getValue().getType() == Type.ANY){
-		return new Entity(((Entity)(value.getValue())).getRealValue());
-	}
-	else{
-		return null;
-	}
+if(getValue().getType()== Type.DOUBLE){
+return new DoubleLiteral(((DoubleLiteral)(value.getValue())).getRealValue());
+}
+else if(getValue().getType()== Type.BOOLEAN){
+return new BooleanLiteral(((BooleanLiteral)(value.getValue())).getRealValue());
+}
+else if(getValue().getType() == Type.ANY){
+return new Entity(((Entity)(value.getValue())).getRealValue());
+}
+else{
+return null;
+}
 }
 
 @Override
 public boolean hasAsSubEntry(IEntry subEntry) {
-	return getVariableValue().hasAsSubEntry(subEntry);
+return getValue().hasAsSubEntry(subEntry);
 }
 
 
 @Override
 public boolean isTypeChecked(){
-	return getVariableValue().isTypeChecked();
+return getValue().isTypeChecked();
 }
 public void setType(Type type){
-	if(Type.isValidType(type))
-		this.type = type;
+if(Type.isValidType(type))
+this.type = type;
 }
 public Type getType(){
-	return type;
+return type;
 }
 
 @Override
 public void setProgram(Program program){
-	super.setProgram(program);
-	getValue().setProgram(program);
-	
-	if(getVariableValue() instanceof Variable){
-		setValue(program.getVariable(((Variable)getVariableValue()).getName()));
-	}
+super.setProgram(program);
+
+if(getValue() != null){
+getValue().setProgram(program);
+
+if(getValue() instanceof Variable){
+setValue(program.getVariable(((Variable)getValue()).getName()));
+}
+}
 }
 
 
 }
-
